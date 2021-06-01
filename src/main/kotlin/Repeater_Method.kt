0aada70d389468d8babefa875c48393c07d940a0
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-06-01 13:20:45 -0300

    alts need doing

--- src/main/kotlin/Repeater_Method.kt
+++
@@ -1,9 +0,0 @@
-fun main(){
-    println("lets get started with method repeat:")
-
-    repeat(3){
-        println("\nThis phase repeat 3 times!")
-    }
-
-    //Have a nice coding! ;)
-}
---
+++ src/main/kotlin/Section_1.6.kt
@@ -0,0 +1,14 @@
+/*
+    The arguments passed from the console can be received in the Kotlin program and it can be used as an input. You
+can pass N (1 2 3 and so on) numbers of arguments from the command prompt.
+
+    A simple example of a command-line argument in Kotlin.
+ */
+
+fun main(){
+    println("Input 2 numbers Int: ")
+    var (a, b) = readLine()!!.split(',')
+
+    println("The max number is: ${maxOf(a.toInt(), b.toInt())}")
+    println("The min number is: ${minOf(a.toInt(), b.toInt())}")
+}
